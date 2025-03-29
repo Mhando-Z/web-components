66195982d6f7f7c71dd1ec54146bf8419a8c3ca0
Mhando-Z <Mhandosz17@gmail.com>
2025-03-29 12:21:46 +0300

    Phase One

---
+++ src/app/dashboard/buttons/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+function Buttons() {
+  return (
+    <div className=" flex flex-col min-h-screen">
+      <div>
+        <h1 className="text-xl"> Mhando zuberi</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Buttons;
---
+++ src/app/dashboard/home/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DashHome() {
+  return (
+    <div className="flex flex-col bg-black">
+      <div>DashHome</div>
+    </div>
+  );
+}
+
+export default DashHome;

f3c7d2ee375b6f1e0ad84795bbe10208cd853c89
Mhando-Z <Mhandosz17@gmail.com>
2025-12-11 19:16:07 +0300

    Phase One

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="bg-linear-to-b min-h-screen from-slate-900 via-blue-950 to-blue-950">
+      <div></div>
+    </div>
+  );
 }
 
 export default page;
